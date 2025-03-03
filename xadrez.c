83517d8a4cdbd0e6bab84548be1f3d00731d9440
Gabriel Ferreira <46980591+Capivarazilla@users.noreply.github.com>
2025-03-03 21:47:55 +0000

    Desafio xadrez (tema 3) nível mestre concluido

--- xadrez.c
+++ xadrez.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
-int main(){
-    int torre = 1, bispo = 1, rainha = 1, mov_cav = 1;
-
-    //Torre se move 5 casas para a direita
-    printf("Movimentos da Torre:\n");
-    for(torre; torre <= 5; torre++){
+//Função para movimento da torre
+void mov_torre (int n_torre) {
+    for(; n_torre > 0; n_torre--){
         printf("Direita\n");
     }
+}
 
-    //Bispo se move 5 casas para a diagonal cima,direita
-    printf("\nMovimentos do Bispo\n");
-    while(bispo <= 5){
-        printf("Cima, Direita\n");
-        bispo++;
+//Função para movimento do bispo
+void mov_bispo(int n_bispo) {
+    for (; n_bispo > 0; n_bispo--)
+    {
+        int temp = 1;
+        while (temp--)
+        {
+           printf("Cima, ");
+        }
+        printf("Direita\n");
+        
     }
+}
 
-    //Rainha se move 8 casas para a esquerda
-    printf("\nMovimentos da Rainha\n");
+//Função para movimento da rainha
+void mov_rainha(int n_rainha) {
     do{
         printf("Esquerda\n");
-        rainha++;
-    } while (rainha <= 8);
+        n_rainha--;
+    } while (n_rainha > 0);
     
-    //Move o cavalo para baixo e esquerda
-    printf("\nMovimentos do Cavalo\n");
-    for (int cavalo1 = 0; cavalo1 < 1; cavalo1++)
+}
+
+//Função para movimento do cavalo
+void mov_cavalo(int n_cavalo){
+    for (int ncavalo = 0; ncavalo < 1; ncavalo++)
     {
         int temp = 2;
         while (temp--)
@@ -35,7 +42,28 @@ int main(){
         printf("Esquerda\n");
         
     }
+}
+
+
+int main(){
+    int torre = 5, bispo = 5, rainha = 8, cavalo = 1;
+
+    //Torre se move 5 casas para a direita
+    printf("Movimentos da Torre:\n");
+    mov_torre(torre);
+
+    //Bispo se move 5 casas para a diagonal cima,direita
+    printf("\nMovimentos do Bispo\n");
+    mov_bispo(bispo);
+
+    //Rainha se move 8 casas para a esquerda
+    printf("\nMovimentos da Rainha\n");
+    mov_rainha(rainha);
+    
+    //Move o cavalo para baixo e esquerda
+    printf("\nMovimentos do Cavalo\n");
+    mov_cavalo(cavalo);
     
 
     return 0;
-}
+    }
