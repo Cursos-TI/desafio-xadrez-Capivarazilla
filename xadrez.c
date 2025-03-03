b22a5e0bc530bf74397fbfe4249c1374fdf6ffcb
Gabriel Ferreira <46980591+Capivarazilla@users.noreply.github.com>
2025-03-03 20:51:46 +0000

    Desafio xadrez.c nível aventureiro concluido

--- xadrez.c
+++ xadrez.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 
 int main(){
-    int i = 1, j = 1, k = 1;
+    int torre = 1, bispo = 1, rainha = 1, mov_cav = 1;
 
+    //Torre se move 5 casas para a direita
     printf("Movimentos da Torre:\n");
-    for(i; i <= 5; i++){
+    for(torre; torre <= 5; torre++){
         printf("Direita\n");
     }
 
+    //Bispo se move 5 casas para a diagonal cima,direita
     printf("\nMovimentos do Bispo\n");
-    while(j <= 5){
+    while(bispo <= 5){
         printf("Cima, Direita\n");
-        j++;
+        bispo++;
     }
+
+    //Rainha se move 8 casas para a esquerda
     printf("\nMovimentos da Rainha\n");
     do{
         printf("Esquerda\n");
-        k++;
-    } while (k <= 8);
+        rainha++;
+    } while (rainha <= 8);
+    
+    //Move o cavalo para baixo e esquerda
+    printf("\nMovimentos do Cavalo\n");
+    for (int cavalo1 = 0; cavalo1 < 1; cavalo1++)
+    {
+        int temp = 2;
+        while (temp--)
+        {
+           printf("Baixo, ");
+        }
+        printf("Esquerda\n");
+        
+    }
     
 
     return 0;
